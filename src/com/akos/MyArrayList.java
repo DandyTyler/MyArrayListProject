d78f0039eff59ac0659487cbac1c4c1b261185d9
AlexK <linki64rus@gmail.com>
2017-11-06 03:46:24 +0300

    Just some code style changes.

--- src/com/akos/MyArrayList.java
+++ src/com/akos/MyArrayList.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MyArrayList <E extends Comparable> implements MyList<E> {
-    private final int ARRAY_START_SIZE= 5;
-    private Object[] array ;
-    private int size =0;
+// TODO: 06.11.2017 Add checkIndex method
+public class MyArrayList<E extends Comparable> implements MyList<E> {
+    private final int ARRAY_START_SIZE = 5;
+    private Object[] array;
+    private int size = 0;
 
-    MyArrayList(){
+    public MyArrayList() {
         this.array = new Object[ARRAY_START_SIZE];
     }
 
@@ -18,20 +19,20 @@ public class MyArrayList <E extends Comparable> implements MyList<E> {
      * Добавляет элемент в массив. Если в массиве недостаточно меса для вставки нового элемента, создается массив
      * большего размера при помощи метода resize(). При добавлении элемент ставится на позицию, необходимую для
      * поддержания отсортированности. Если в массиве еще нет элементов, добавляется по индексу 0;
-     * @param element  Элемент добавляемый в массив.
+     *
+     * @param element Элемент добавляемый в массив.
      */
     public void add(E element) {
         if ((array.length) <= (size + 1)) {
             resize();
         }
-        if(size==0){
+        if (size == 0) {
             array[size++] = element;
-        }
-        else {
+        } else {
             int buff = size;
-            for(int i = 0; i<=buff-1; i++){
+            for (int i = 0; i <= buff - 1; i++) {
                 //Если новый элемент меньше старого, он вставляется перед старым, следовательно размер массива увеличивается
-                if (element.equals(minOf(element, (E)array[i]))){
+                if (element.equals(minOf(element, (E) array[i]))) {
                     add(i, element);
                     break;
                 }
@@ -39,66 +40,65 @@ public class MyArrayList <E extends Comparable> implements MyList<E> {
             // Если увеличения массива не произошло, элемент добавляется в конец, т.к. он больше всех остальных элементов
             if (buff == size)
                 array[size++] = element;
-        };
-
+        }
     }
 
     /**
      * Добавляет элемент по индексу, сдвигая старые элементы вправо
      * Private, т.к. используется только для вставки нового элемента в порядке сортировки
-     * @param index позиция по которой вставляется элемент
-     * @param element  Элемент добавляемый в массив
+     *
+     * @param index   позиция по которой вставляется элемент
+     * @param element Элемент добавляемый в массив
      */
-    private void add(int index,E element) {
-        if (array.length <= index+1){
+    private void add(int index, E element) {
+        if (array.length <= index + 1) {
             resize();
         }
         System.arraycopy(array, index, array, index + 1, size - index);
-        array[index]= element;
+        array[index] = element;
         size++;
-
     }
-    private E minOf(E value1, E value2){
-        Comparable buff = (Comparable) value1;
-        if (buff.compareTo(value2)<0){
+
+    private E minOf(E value1, E value2) {
+        Comparable buff = value1;
+        if (buff.compareTo(value2) < 0) {
             return value1;
-        }
-        else
+        } else
             return value2;
     }
 
-    private E maxOf(E value1, E value2){
-        Comparable buff = (Comparable) value1;
-        if (buff.compareTo(value2)>=0){
+    private E maxOf(E value1, E value2) {
+        Comparable buff = value1;
+        if (buff.compareTo(value2) >= 0) {
             return value1;
-        }
-        else
+        } else
             return value2;
     }
 
-    public E get(){
-        return (E)array[size -1];
+    public E get() {
+        return (E) array[size - 1];
     }            //Без аргументов, наверное не нужен
 
-    public E get(int index){
-        return (E)array[index];
+    public E get(int index) {
+        return (E) array[index];
     }
 
-        public E remove(){                                  //Без аргументов, наверное не нужен
-        E oldValue = (E)array[--size];
+    public E remove() {                                  //Без аргументов, наверное не нужен
+        E oldValue = (E) array[--size];
         return oldValue;
     }
 
     /**
      * Удаляет элемент по индексу
-     * @
+     *
      * @param index индекс удаляемого элемента
      * @return Возврвщает удаленный элемент
+     * @
      */
-    public E remove(int index){
-        E oldValue = (E)array[index];
-        System.arraycopy(array,index+1, array, index, size - (index+1));
-        array[size]=null;
+    public E remove(int index) {
+        E oldValue = (E) array[index];
+        System.arraycopy(array, index + 1, array, index, size - (index + 1));
+        array[size] = null;
         size--;
         return oldValue;
     }
@@ -106,11 +106,11 @@ public class MyArrayList <E extends Comparable> implements MyList<E> {
     /**
      * Увеличивает размер массива
      */
-    private void resize(){
-        array = Arrays.copyOf(array, array.length*2);
+    private void resize() {
+        array = Arrays.copyOf(array, array.length * 2);
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
@@ -119,18 +119,18 @@ public class MyArrayList <E extends Comparable> implements MyList<E> {
         return new MyListIterator();
     }
 
-    private class MyListIterator implements Iterator<E>{
+    private class MyListIterator implements Iterator<E> {
         private int curr = 0;
 
         @Override
         public boolean hasNext() {
-            return this.curr<size;
+            return this.curr < size;
         }
 
         @Override
         public E next() {
             int nextIndex;
-            if (!this.hasNext()){
+            if (!this.hasNext()) {
                 throw new NoSuchElementException();
             }
             E value = get(curr);
@@ -141,9 +141,9 @@ public class MyArrayList <E extends Comparable> implements MyList<E> {
 
     @Override
     public String toString() {
-        String str="";
-        for (int i = 0; i<this.size; i++)
-            str = str + array[i]+ " ";
+        String str = "";
+        for (int i = 0; i < this.size; i++)
+            str = str + array[i] + " ";
         return str;
     }
 }
--- src/com/akos/MyList.java
+++ src/com/akos/MyList.java
@@ -1,9 +1,12 @@
 package com.akos;
 
-public interface MyList <E>  extends Iterable<E> {
+public interface MyList<E> extends Iterable<E> {
 
     int size();
+
     void add(E element);
+
     E get(int index);
+
     E remove(int index);
 }
